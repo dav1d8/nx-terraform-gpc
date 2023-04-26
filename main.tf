b19dc37064478798395c772ccf880d87ecc468a8
dav1d8 <baranyai8david@gmail.com>
2023-04-26 18:40:50 +0200

    Next 2

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 terraform {
+  backend "gcs" {
+    bucket = "terraform-state-345678976543"
+    prefix = "terraform/state"
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -8,8 +12,6 @@ terraform {
 }
 
 provider "google" {
-  #credentials = file("credentials.json")
-
   project = var.project
   region  = var.region
   zone    = var.zone
@@ -20,31 +22,31 @@ resource "google_service_account" "default" {
   display_name = "Service Account (nx)"
 }
 
-#resource "google_container_cluster" "primary" {
-#  name     = "nx-cluster"
-#  location = var.region
-#
-#  initial_node_count       = 1
-#  remove_default_node_pool = true
-#}
-#
-#resource "google_container_node_pool" "primary_nodes" {
-#  name       = "nx-node-pool"
-#  location   = var.region
-#  cluster    = google_container_cluster.primary.name
-#  node_count = 1
-#
-#  node_locations = [var.zone]
-#
-#  node_config {
-#    machine_type = "e2-small"
-#
-#    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-#    service_account = google_service_account.default.email
-#    oauth_scopes = [
-#      "https://www.googleapis.com/auth/cloud-platform"
-#    ]
-#
-#    disk_size_gb = 16
-#  }
-#}
+resource "google_container_cluster" "primary" {
+  name     = "nx-cluster"
+  location = var.region
+
+  initial_node_count       = 1
+  remove_default_node_pool = true
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "nx-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_locations = [var.zone]
+
+  node_config {
+    machine_type = "e2-small"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    disk_size_gb = 16
+  }
+}
